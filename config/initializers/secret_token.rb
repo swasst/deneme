6882bda277a8074c3d892c9b16252e59f5a71fb9
Arzum Karatas <arzum.karatas@gediz.edu.tr>
2014-11-25 10:51:43 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deneme::Application.config.secret_key_base = '6a02532ddb8efbf63c8240dd78b680b177646a090f39abd2dadce0af65312419a3349614358deb9cead651df73c0bb120615fc5dd7d5ae14ef5d4a293c500ea8'
